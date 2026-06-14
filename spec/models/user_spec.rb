3441e2f26c645061fd0525546f9a3d0c2ae5a933
officialkatob510-oss <official.kato.b510@gmail.com>
2026-06-14 09:44:52 +0900

    ユーザー管理機能の修正
    エラー表示修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe User, type: :model do
       user.password = 'abcdef'
       user.password_confirmation = 'abcdef'
       user.valid?
-      expect(user.errors.full_messages).to include("Password is invalid")
+      expect(user.errors.full_messages).to include('Password is invalid')
     end
 
     it 'passwordが数字のみでは登録できない' do
@@ -49,10 +49,10 @@ RSpec.describe User, type: :model do
       user.password = '123456'
       user.password_confirmation = '123456'
       user.valid?
-      expect(user.errors.full_messages).to include("Password is invalid")
+      expect(user.errors.full_messages).to include('Password is invalid')
     end
 
-     it 'last_nameが空では登録できない' do
+    it 'last_nameが空では登録できない' do
       user = FactoryBot.build(:user)
       user.last_name = ''
       user.valid?
@@ -73,7 +73,7 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to include("Last name kana can't be blank")
     end
 
-     it 'first_name_kanaが空では登録できない' do
+    it 'first_name_kanaが空では登録できない' do
       user = FactoryBot.build(:user)
       user.first_name_kana = ''
       user.valid?
@@ -87,32 +87,32 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to include("Birth date can't be blank")
     end
 
-     it 'last_nameが半角英字では登録できない' do
+    it 'last_nameが半角英字では登録できない' do
       user = FactoryBot.build(:user)
       user.last_name = 'yamada'
       user.valid?
-      expect(user.errors.full_messages).to include("Last name is invalid")
+      expect(user.errors.full_messages).to include('Last name is invalid')
     end
 
-     it 'first_nameが半角英字では登録できない' do
+    it 'first_nameが半角英字では登録できない' do
       user = FactoryBot.build(:user)
       user.first_name = 'taro'
       user.valid?
-      expect(user.errors.full_messages).to include("First name is invalid")
+      expect(user.errors.full_messages).to include('First name is invalid')
     end
 
-     it 'last_name_kanaがひらがなでは登録できない' do
+    it 'last_name_kanaがひらがなでは登録できない' do
       user = FactoryBot.build(:user)
       user.last_name_kana = 'やまだ'
       user.valid?
-      expect(user.errors.full_messages).to include("Last name kana is invalid")
+      expect(user.errors.full_messages).to include('Last name kana is invalid')
     end
 
-      it 'first_name_kanaがひらがなでは登録できない' do
+    it 'first_name_kanaがひらがなでは登録できない' do
       user = FactoryBot.build(:user)
       user.first_name_kana = 'たろう'
       user.valid?
-      expect(user.errors.full_messages).to include("First name kana is invalid")
+      expect(user.errors.full_messages).to include('First name kana is invalid')
     end
   end
 end
